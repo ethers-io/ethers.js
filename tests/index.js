0694a5db0e535a0e2995dfff5e0415ab11fc37f7
ricmoo <me@ricmoo.com>
2017-02-24 15:24:11 -0500

    Added auto-running testcases.

--- tests/index.js
+++ tests/index.js
@@ -1,44 +1,20 @@
 'use strict';
 
-console.log('Running test cases... (this can take a long time, please be patient)');
+var reporter = require('nodeunit').reporters.default;
 
+/*
+module.exports = {};
 
-// Test converting private keys to addresses
-module.exports.testPrivateKeyToAddress = require('./test-privatekey.js');
-
-// Test checksum addresses
-module.exports.testChecksumAddress = require('./test-checksum-address.js');
-
-// Test ICAP addresses
-module.exports.testIcapAddress = require('./test-icap-address.js');
-
-
-// Test ether strng formatting and parsing
-module.exports.testEtherFormat = require('./test-ether-format.js');
-
-
-// Test transactions
-module.exports.testTrasactions = require('./test-transactions.js');
-
-
-// Test Solidity encoding/decoding parameters
-module.exports.testSolidityCoder = require('./test-solidity-coder.js');
-
-// Test the contract meta-class
-module.exports.testContracts = require('./test-contracts.js');
-
-
-// Test the secret storage JSON wallet encryption/decryption
-module.exports.testSecretStorage = require('./test-secret-storage.js');
-
-// Test brain wallet generation
-module.exports.testBrainWallet = require('./test-brain-wallet.js');
-
-
-// Test contract address helper
-module.exports.testContractAddress = require('./test-contract-address.js');
-
-
-// Test the providers API (we still need to add a lot ore test cases here)
-module.exports.testProviders = require('./test-providers.js');
-
+['contract-iterface', 'hdnode', 'utils', 'wallet'].forEach(function(runName) {
+    var runs = require('./run-' + runName + '.js');
+    for (var testcaseName in runs) {
+        module.exports[testcaseName] = runs[testcaseName];
+    }
+});
+*/
+reporter.run([
+    'run-contract-iterface.js'
+    'hdnode',
+    'utils',
+    'wallet',
+]);
