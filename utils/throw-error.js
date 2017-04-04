807b27048bc2bce6d819ac4f56a74e26d1cf4bbe
ricmoo <me@ricmoo.com>
2017-04-04 17:06:49 -0400

    Added throw-error utility.

---
+++ utils/throw-error.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function throwError(message, params) {
+    var error = new Error(message);
+    for (var key in params) {
+        error[key] = params[key];
+    }
+    throw error;
+}
+
+module.exports = throwError;
