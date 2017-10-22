4d002150988894cc01073ae4da2e06bccfc4565b
Richard Moore <me@ricmoo.com>
2017-10-22 17:56:01 -0400

    Added all test cases for PhantomJS.

--- tests/browser.js
+++ tests/browser.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-//    account: require('./test-account'),
-//    contractInterface: require('./test-contract-interface'),
-//    hdnode: require('./test-hdnode'),
+    account: require('./test-account'),
+    contractInterface: require('./test-contract-interface'),
+    hdnode: require('./test-hdnode'),
     utils: require('./test-utils'),
-//    wallet: require('./test-wallet'),
+    wallet: require('./test-wallet'),
 }
