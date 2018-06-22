63e5ad1d95ab33b4953cb23ea9c9f69b43e168f7
Richard Moore <github@ricmoo.com>
2018-06-22 02:08:52 -0400

    Removed deprecated wallet operations (brainwallets and bare send).

--- src.ts/wallet/wallet.ts
+++ src.ts/wallet/wallet.ts
@@ -1,7 +1,5 @@
 'use strict';
 
-import scrypt from 'scrypt-js';
-
 import { defaultPath, entropyToMnemonic, fromMnemonic, HDNode } from './hdnode';
 import * as secretStorage from './secret-storage';
 import { ProgressCallback } from './secret-storage';
@@ -10,22 +8,15 @@ import { recoverAddress, SigningKey } from './signing-key';
 import { BlockTag, Provider, TransactionRequest, TransactionResponse } from '../providers/provider';
 import { Wordlist } from '../wordlists/wordlist';
 
-import { BigNumber, BigNumberish } from '../utils/bignumber';
+import { BigNumber } from '../utils/bignumber';
 import { arrayify, Arrayish, concat, hexlify, joinSignature } from '../utils/bytes';
 import { hashMessage } from '../utils/hash';
 import { keccak256 } from '../utils/keccak256';
 import { defineReadOnly, resolveProperties, shallowCopy } from '../utils/properties';
 import { randomBytes } from '../utils/random-bytes';
 import { sign as signTransaction } from '../utils/transaction';
-import { toUtf8Bytes, UnicodeNormalizationForm } from '../utils/utf8';
 
 import * as errors from '../utils/errors';
-
-
-// This ensures we inject a setImmediate into the global space, which
-// dramatically improves the performance of the scrypt PBKDF.
-console.log("Fix this! Setimmediate");
-//import _setimmediate = require('setimmediate');
 
 export abstract class Signer {
     provider?: Provider;
@@ -130,19 +121,6 @@ export class Wallet extends Signer {
         return this.provider.sendTransaction(this.sign(tx));
     }
 
-    send(addressOrName: string, amountWei: BigNumberish, options: any): Promise<TransactionResponse> {
-        if (!options) { options = {}; }
-
-        return this.sendTransaction({
-            to: addressOrName,
-            gasLimit: options.gasLimit,
-            gasPrice: options.gasPrice,
-            nonce: options.nonce,
-            value: amountWei,
-        });
-    }
-
-
     encrypt(password: Arrayish | string, options: any, progressCallback: ProgressCallback): Promise<string> {
         if (typeof(options) === 'function' && !progressCallback) {
             progressCallback = options;
@@ -186,7 +164,7 @@ export class Wallet extends Signer {
         return Wallet.fromMnemonic(mnemonic, options.path, options.locale);
     }
 
-    static fromEncryptedWallet(json: string, password: Arrayish, progressCallback: ProgressCallback): Promise<Wallet> {
+    static fromEncryptedJson(json: string, password: Arrayish, progressCallback: ProgressCallback): Promise<Wallet> {
         if (progressCallback && typeof(progressCallback) !== 'function') {
             throw new Error('invalid callback');
         }
@@ -225,39 +203,6 @@ export class Wallet extends Signer {
     static fromMnemonic(mnemonic: string, path?: string, wordlist?: Wordlist): Wallet {
         if (!path) { path = defaultPath; }
         return new Wallet(fromMnemonic(mnemonic, wordlist).derivePath(path));
-    }
-
-
-    static fromBrainWallet(username: Arrayish | string, password: Arrayish | string, progressCallback: ProgressCallback): Promise<Wallet> {
-        if (progressCallback && typeof(progressCallback) !== 'function') {
-            throw new Error('invalid callback');
-        }
-
-        if (typeof(username) === 'string') {
-            username =  toUtf8Bytes(username, UnicodeNormalizationForm.NFKC);
-        } else {
-            username = arrayify(username);
-        }
-
-        if (typeof(password) === 'string') {
-            password =  toUtf8Bytes(password, UnicodeNormalizationForm.NFKC);
-        } else {
-            password = arrayify(password);
-        }
-
-        return new Promise(function(resolve, reject) {
-            scrypt(password, username, (1 << 18), 8, 1, 32, function(error, progress, key) {
-                if (error) {
-                    reject(error);
-
-                } else if (key) {
-                    resolve(new Wallet(hexlify(key)));
-
-                } else if (progressCallback) {
-                    return progressCallback(progress);
-                }
-            });
-        });
     }
 
 
