5a0607d11b2d25505ca78fdaa0975501ed490e4f
Richard Moore <github@ricmoo.com>
2018-07-23 19:23:53 -0400

    Updated dist files.

--- dist/types/_version.d.ts
+++ dist/types/_version.d.ts
@@ -1,2 +1,2 @@
-export declare const version = "4.0.0-beta.2";
+export declare const version = "4.0.0-beta.3";
 //# sourceMappingURL=_version.d.ts.map
--- src.ts/_version.ts
+++ src.ts/_version.ts
@@ -1,1 +1,1 @@
-export const version = "4.0.0-beta.2";
+export const version = "4.0.0-beta.3";
