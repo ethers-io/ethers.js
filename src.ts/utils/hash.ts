66440b8542e80e056bd96d94a1be9fcbb1c9b2d7
Richard Moore <github@ricmoo.com>
2018-12-10 17:02:02 -0500

    Better error messages for namehash (#364).

--- src.ts/utils/hash.ts
+++ src.ts/utils/hash.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import * as errors from '../errors';
+
 import { concat, hexlify } from './bytes';
 import { toUtf8Bytes } from './utf8';
 import { keccak256 } from './keccak256';
@@ -11,11 +13,18 @@ import { Arrayish } from './bytes';
 
 ///////////////////////////////
 
-var Zeros = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-var Partition = new RegExp("^((.*)\\.)?([^.]+)$");
-var UseSTD3ASCIIRules = new RegExp("^[a-z0-9.-]*$");
+const Zeros = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+const Partition = new RegExp("^((.*)\\.)?([^.]+)$");
+const UseSTD3ASCIIRules = new RegExp("^[a-z0-9.-]*$");
 
 export function namehash(name: string): string {
+    if (typeof(name) !== 'string') {
+        errors.throwError('invalid address - ' + String(name), errors.INVALID_ARGUMENT, {
+            argument: 'name',
+            value: name
+        });
+    }
+
     name = name.toLowerCase();
 
     // Supporting the full UTF-8 space requires additional (and large)
@@ -23,13 +32,16 @@ export function namehash(name: string): string {
     // It should be fairly easy in the future to support systems with
     // String.normalize, but that is future work.
     if (!name.match(UseSTD3ASCIIRules)) {
-        throw new Error('contains invalid UseSTD3ASCIIRules characters');
+        errors.throwError('contains invalid UseSTD3ASCIIRules characters', errors.INVALID_ARGUMENT, {
+            argument: 'name',
+            value: name
+        });
     }
 
-    var result: string | Uint8Array = Zeros;
+    let result: string | Uint8Array = Zeros;
     while (name.length) {
-        var partition = name.match(Partition);
-        var label = toUtf8Bytes(partition[3]);
+        let partition = name.match(Partition);
+        let label = toUtf8Bytes(partition[3]);
         result = keccak256(concat([result, keccak256(label)]));
 
         name = partition[2] || '';
@@ -44,11 +56,10 @@ export function id(text: string): string {
 }
 
 export function hashMessage(message: Arrayish | string): string {
-    var payload = concat([
+    return keccak256(concat([
         toUtf8Bytes('\x19Ethereum Signed Message:\n'),
         toUtf8Bytes(String(message.length)),
         ((typeof(message) === 'string') ? toUtf8Bytes(message): message)
-    ]);
-    return keccak256(payload);
+    ]));
 }
 
