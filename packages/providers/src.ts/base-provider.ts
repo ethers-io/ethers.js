1e72fc7d6f7c3be4410dbdcfbab9a0463ceb52bd
Richard Moore <github@ricmoo.com>
2020-01-07 19:41:43 -0500

    Fixed resolveName when name is an address with an invalid checksum (#694).

--- packages/providers/src.ts/base-provider.ts
+++ packages/providers/src.ts/base-provider.ts
@@ -835,7 +835,10 @@ export class BaseProvider extends Provider {
         // If it is already an address, nothing to resolve
         try {
             return Promise.resolve(this.formatter.address(name));
-        } catch (error) { }
+        } catch (error) {
+            // If is is a hexstring, the address is bad (See #694)
+            if (isHexString(name)) { throw error; }
+        }
 
         // Get the addr from the resovler
         const resolverAddress = await this._getResolver(name);
