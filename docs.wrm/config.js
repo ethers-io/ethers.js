067d96062a690b06636c51e9cac81293e42b6ca1
Richard Moore <github@ricmoo.com>
2020-02-17 17:56:13 -0500

    Updated documentation source.

--- docs.wrm/config.js
+++ docs.wrm/config.js
@@ -158,6 +158,7 @@ module.exports = {
       "link-jsonrpc": "https:/\/github.com/ethereum/wiki/wiki/JSON-RPC",
       "link-mit": "https:/\/en.m.wikipedia.org/wiki/MIT_License",
       "link-namehash": "https:/\/docs.ens.domains/contract-api-reference/name-processing#hashing-names",
+      "link-rlp": { name: "Recursive Length Prefix", url: "https:/\/github.com/ethereum/wiki/wiki/RLP" },
 
       "link-ethersio": "https:/\/ethers.io/",
       "link-ethers-docs": "https:/\/docs.ethers.io/",
@@ -165,17 +166,26 @@ module.exports = {
       "link-ethers-npm": "https:/\/www.npmjs.com/search?q=%40ethersproject%2F",
       "link-ethers-asm-grammar": "https:/\/github.com/ethers-io/ethers.js/blob/ethers-v5-beta/packages/asm/grammar.jison",
 
-      "link-eip-155": "https:/\/eips.ethereum.org/EIPS/eip-155",
+      "link-eip-155": { name: "EIP-155", url: "https:/\/eips.ethereum.org/EIPS/eip-155" },
       "link-eip-609": "https:/\/eips.ethereum.org/EIPS/eip-609",
       "link-eip-1014": "https:/\/eips.ethereum.org/EIPS/eip-1014",
       "link-eip-2098": "https:/\/eips.ethereum.org/EIPS/eip-2098",
+      "link-bip-39": "https://en.bitcoin.it/wiki/BIP_0039",
+      "link-bip-32": "https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki",
+
+      "link-npm-elliptic": { name: "elliptic", url: "https:/\/www.npmjs.com/package/elliptic" },
+      "link-npm-bnjs": { name: "BN.js", url: "https:/\/www.npmjs.com/package/bn.js" },
+      "link-npm-query-bignumber": "https:/\/www.npmjs.com/search?q=bignumber",
 
       "link-js-array": "https:/\/developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
       "link-js-bigint": "https:/\/developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt",
+      "link-js-maxsafe": "https:/\/developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER#Description",
       "link-js-typedarray": "https:/\/developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray",
 
       "link-ricmoo-humanreadableabi": "https:/\/blog.ricmoo.com/human-readable-contract-abis-in-ethers-js-141902f4d917",
 
+      "link-wiki-basicauth": { name: "Basic Authentication", url: "https:/\/en.wikipedia.org/wiki/Basic_access_authentication" },
+      "link-wiki-backoff": { name: "Exponential Backoff", url: "https:/\/en.wikipedia.org/wiki/Exponential_backoff" },
       "link-wiki-bloomfilter": "https:/\/en.wikipedia.org/wiki/Bloom_filter",
       "link-wiki-cryptographichash": "https:/\/en.wikipedia.org/wiki/Cryptographic_hash_function",
       "link-wiki-homoglyph": "https:/\/en.wikipedia.org/wiki/IDN_homograph_attack",
@@ -190,6 +200,7 @@ module.exports = {
       "link-wiki-utf8-replacement": "https:/\/en.wikipedia.org/wiki/Specials_%28Unicode_block%29#Replacement_character",
       "link-wiki-scrypt": "https:/\/en.wikipedia.org/wiki/Scrypt",
       "link-wiki-sha3": "https:/\/en.wikipedia.org/wiki/SHA-3",
-      
+      "link-wiki-overflow": { name: "overflow", url: "https:/\/en.wikipedia.org/wiki/Integer_overflow" },
+      "link-wiki-underflow": { name: "arithmetic underflow", url: "https:/\/en.wikipedia.org/wiki/Arithmetic_underflow" },
   }
 };
