27b3af0a7b15d260a14f249447b81bfbeb6a3597
Richard Moore <github@ricmoo.com>
2020-09-17 17:17:15 -0400

    Removed dead files.

--- packages/testcases/lib.esm/generation-scripts/nameprep.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-import fs from "fs";
-import { resolve } from "path";
-import { saveTests } from "..";
-const testcases = JSON.parse(fs.readFileSync(resolve(__dirname, "../input/nameprep-josefsson-idn.json")).toString());
-saveTests("nameprep", testcases);
