4306b3563a171baa9d7bf4872475a13c3434f834
Richard Moore <github@ricmoo.com>
2020-09-23 00:00:21 -0400

    Updated CLI solc to versin 0.7.1.

--- packages/tests/contracts/test-solc/consumer.sol
+++ packages/tests/contracts/test-solc/consumer.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.7.1;
 
 import "./libraries/lib.sol";
 
--- packages/tests/contracts/test-solc/libraries/lib.sol
+++ packages/tests/contracts/test-solc/libraries/lib.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.7.1;
 
 library Lib {
   function f() internal returns (uint) {
