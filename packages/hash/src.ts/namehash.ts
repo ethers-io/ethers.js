3e676f21b00931ed966f4561e4f28792a1f8f154
Richard Moore <github@ricmoo.com>
2020-10-18 21:45:32 -0400

    Split hash library files up.

---
+++ packages/hash/src.ts/namehash.ts
@@ -0,0 +1,44 @@
+import { concat, hexlify } from "@ethersproject/bytes";
+import { nameprep, toUtf8Bytes } from "@ethersproject/strings";
+import { keccak256 } from "@ethersproject/keccak256";
+
+import { Logger } from "@ethersproject/logger";
+import { version } from "./_version";
+const logger = new Logger(version);
+
+const Zeros = new Uint8Array(32);
+Zeros.fill(0);
+
+const Partition = new RegExp("^((.*)\\.)?([^.]+)$");
+
+export function isValidName(name: string): boolean {
+    try {
+        const comps = name.split(".");
+        for (let i = 0; i < comps.length; i++) {
+            if (nameprep(comps[i]).length === 0) {
+                throw new Error("empty")
+            }
+        }
+        return true;
+    } catch (error) { }
+    return false;
+}
+
+export function namehash(name: string): string {
+    /* istanbul ignore if */
+    if (typeof(name) !== "string") {
+        logger.throwArgumentError("invalid address - " + String(name), "name", name);
+    }
+
+    let result: string | Uint8Array = Zeros;
+    while (name.length) {
+        const partition = name.match(Partition);
+        const label = toUtf8Bytes(nameprep(partition[3]));
+        result = keccak256(concat([result, keccak256(label)]));
+
+        name = partition[2] || "";
+    }
+
+    return hexlify(result);
+}
+
