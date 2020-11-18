4b7028a66128b2fdf0110b44368a81d5b05e7149
Richard Moore <github@ricmoo.com>
2020-11-18 17:25:58 -0500

    docs: Typo in BigNumber source (#1100).

--- packages/bignumber/src.ts/bignumber.ts
+++ packages/bignumber/src.ts/bignumber.ts
@@ -4,7 +4,7 @@
  *  BigNumber
  *
  *  A wrapper around the BN.js object. We use the BN.js library
- *  because it is used by elliptic, so it is required regardles.
+ *  because it is used by elliptic, so it is required regardless.
  *
  */
 
