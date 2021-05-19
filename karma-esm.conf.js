d395d16fa357ec5dda9b59922cf21c39dc34c071
Richard Moore <github@ricmoo.com>
2021-05-19 01:30:12 -0400

    admin: flags for karma to prevent timeout

--- karma-esm.conf.js
+++ karma-esm.conf.js
@@ -35,7 +35,12 @@ module.exports = function(config) {
           // Cloudflare will block (on the testnet endpoints) any traffic
           // from a headless chome (based on the user agent), so we lie
           // This was take from Safari, because that is what I had on-hand
-          '--user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15']
+          '--user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+
+          // https://stackoverflow.com/questions/58484124/karma-disconnectedreconnect-failed-before-timeout-of-with-chromeheadless
+          '--disable-gpu',
+          '--no-sandbox'
+        ],
       }
     },
     /*
