ab989642e68810d057c6b0777a0a757ac4ea37a6
Cezary Falba <cezary@falba.net>
2021-06-25 09:15:40 +0200

    Fixed compilation errors in base-provider with TypeScript 4.3.2

--- packages/providers/src.ts/base-provider.ts
+++ packages/providers/src.ts/base-provider.ts
@@ -16,7 +16,7 @@ import { sha256 } from "@ethersproject/sha2";
 import { toUtf8Bytes, toUtf8String } from "@ethersproject/strings";
 import { poll } from "@ethersproject/web";
 
-import bech32 from "bech32";
+import * as bech32 from "bech32";
 
 import { Logger } from "@ethersproject/logger";
 import { version } from "./_version";
