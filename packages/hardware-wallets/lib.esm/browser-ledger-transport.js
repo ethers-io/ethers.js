bc309704a1dd37585e672caead83a72d959fe488
Astrid Fox <astridfox@protonmail.com>
2021-08-30 13:50:55 +0300

    Update built lib and lib.esm

---
+++ packages/hardware-wallets/lib.esm/browser-ledger-transport.js
@@ -0,0 +1,7 @@
+"use strict";
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
+export var transports = {
+    "webusb": TransportWebUSB,
+    "default": TransportWebUSB
+};
+//# sourceMappingURL=browser-ledger-transport.js.map
--- packages/hardware-wallets/lib.esm/ledger-transport.js
+++ packages/hardware-wallets/lib.esm/ledger-transport.js
@@ -1,7 +1,35 @@
 "use strict";
-import u2f from "@ledgerhq/hw-transport-u2f";
-export const transports = {
-    "u2f": u2f,
-    "default": u2f
-};
+var hidCache = null;
+var hidWrapper = Object.freeze({
+    create: function () {
+        // Load the library if not loaded
+        if (hidCache == null) {
+            hidCache = new Promise(function (resolve, reject) {
+                try {
+                    var hid = require("@ledgerhq/hw-transport-node-hid");
+                    if (hid.create == null) {
+                        resolve(hid["default"]);
+                    }
+                    resolve(hid);
+                }
+                catch (error) {
+                    reject(error);
+                }
+            });
+            /*
+            hidCache = import("@ledgerhq/hw-transport-node-hid").then((hid) => {
+                if (hid.create == null) { return hid["default"]; }
+                return hid;
+            });
+            */
+        }
+        return hidCache.then(function (hid) {
+            return hid.create();
+        });
+    }
+});
+export var transports = Object.freeze({
+    "hid": hidWrapper,
+    "default": hidWrapper
+});
 //# sourceMappingURL=ledger-transport.js.map
