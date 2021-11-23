6a9e6126eefb7c513becc7203b806750735c4402
ljybill <ljybill0215@gmail.com>
2021-11-23 10:37:25 +0800

    fix: Fixed connect error when support 'HTTP:' or 'WSS:'
    `ethers.providers.getDefaultProvider('HTTP://127.0.0.1:7545')` will
    throw Error

--- packages/providers/src.ts/index.ts
+++ packages/providers/src.ts/index.ts
@@ -53,7 +53,7 @@ function getDefaultProvider(network?: Networkish, options?: any): BaseProvider {
         // Handle http and ws (and their secure variants)
         const match = network.match(/^(ws|http)s?:/i);
         if (match) {
-            switch (match[1]) {
+            switch (match[1].toLocaleLowerCase()) {
                 case "http":
                     return new JsonRpcProvider(network);
                 case "ws":
