79e13233f4aa11803e3f5380a285862eab278e2b
fritzschoff <max.j.f@gmx.de>
2021-11-29 13:29:29 +0100

    restored formatting

--- packages/providers/src.ts/infura-provider.ts
+++ packages/providers/src.ts/infura-provider.ts
@@ -13,158 +13,131 @@ const logger = new Logger(version);
 
 import { UrlJsonRpcProvider } from "./url-json-rpc-provider";
 
-const defaultProjectId = "84842078b09946638c03157f83405213";
 
-export class InfuraWebSocketProvider
-  extends WebSocketProvider
-  implements CommunityResourcable
-{
-  readonly apiKey: string;
-  readonly projectId: string;
-  readonly projectSecret: string;
+const defaultProjectId = "84842078b09946638c03157f83405213"
 
-  constructor(network?: Networkish, apiKey?: any) {
-    const provider = new InfuraProvider(network, apiKey);
-    const connection = provider.connection;
-    if (connection.password) {
-      logger.throwError(
-        "INFURA WebSocket project secrets unsupported",
-        Logger.errors.UNSUPPORTED_OPERATION,
-        {
-          operation: "InfuraProvider.getWebSocketProvider()",
+export class InfuraWebSocketProvider extends WebSocketProvider implements CommunityResourcable {
+    readonly apiKey: string;
+    readonly projectId: string;
+    readonly projectSecret: string;
+
+    constructor(network?: Networkish, apiKey?: any) {
+        const provider = new InfuraProvider(network, apiKey);
+        const connection = provider.connection;
+        if (connection.password) {
+            logger.throwError("INFURA WebSocket project secrets unsupported", Logger.errors.UNSUPPORTED_OPERATION, {
+                operation: "InfuraProvider.getWebSocketProvider()"
+            });
         }
-      );
+
+        const url = connection.url.replace(/^http/i, "ws").replace("/v3/", "/ws/v3/");
+        super(url, network);
+
+        defineReadOnly(this, "apiKey", provider.projectId);
+        defineReadOnly(this, "projectId", provider.projectId);
+        defineReadOnly(this, "projectSecret", provider.projectSecret);
     }
 
-    const url = connection.url
-      .replace(/^http/i, "ws")
-      .replace("/v3/", "/ws/v3/");
-    super(url, network);
-
-    defineReadOnly(this, "apiKey", provider.projectId);
-    defineReadOnly(this, "projectId", provider.projectId);
-    defineReadOnly(this, "projectSecret", provider.projectSecret);
-  }
-
-  isCommunityResource(): boolean {
-    return this.projectId === defaultProjectId;
-  }
+    isCommunityResource(): boolean {
+        return (this.projectId === defaultProjectId);
+    }
 }
 
 export class InfuraProvider extends UrlJsonRpcProvider {
-  readonly projectId: string;
-  readonly projectSecret: string;
+    readonly projectId: string;
+    readonly projectSecret: string;
 
-  static getWebSocketProvider(
-    network?: Networkish,
-    apiKey?: any
-  ): InfuraWebSocketProvider {
-    return new InfuraWebSocketProvider(network, apiKey);
-  }
-
-  static getApiKey(apiKey: any): any {
-    const apiKeyObj: {
-      apiKey: string;
-      projectId: string;
-      projectSecret: string;
-    } = {
-      apiKey: defaultProjectId,
-      projectId: defaultProjectId,
-      projectSecret: null,
-    };
-
-    if (apiKey == null) {
-      return apiKeyObj;
+    static getWebSocketProvider(network?: Networkish, apiKey?: any): InfuraWebSocketProvider {
+        return new InfuraWebSocketProvider(network, apiKey);
     }
 
-    if (typeof apiKey === "string") {
-      apiKeyObj.projectId = apiKey;
-    } else if (apiKey.projectSecret != null) {
-      logger.assertArgument(
-        typeof apiKey.projectId === "string",
-        "projectSecret requires a projectId",
-        "projectId",
-        apiKey.projectId
-      );
-      logger.assertArgument(
-        typeof apiKey.projectSecret === "string",
-        "invalid projectSecret",
-        "projectSecret",
-        "[REDACTED]"
-      );
+    static getApiKey(apiKey: any): any {
+        const apiKeyObj: { apiKey: string, projectId: string, projectSecret: string } = {
+            apiKey: defaultProjectId,
+            projectId: defaultProjectId,
+            projectSecret: null
+        };
 
-      apiKeyObj.projectId = apiKey.projectId;
-      apiKeyObj.projectSecret = apiKey.projectSecret;
-    } else if (apiKey.projectId) {
-      apiKeyObj.projectId = apiKey.projectId;
-    }
+        if (apiKey == null) { return apiKeyObj; }
 
-    apiKeyObj.apiKey = apiKeyObj.projectId;
+        if (typeof(apiKey) === "string") {
+            apiKeyObj.projectId = apiKey;
 
-    return apiKeyObj;
-  }
+        } else if (apiKey.projectSecret != null) {
+            logger.assertArgument((typeof(apiKey.projectId) === "string"),
+                "projectSecret requires a projectId", "projectId", apiKey.projectId);
+            logger.assertArgument((typeof(apiKey.projectSecret) === "string"),
+                "invalid projectSecret", "projectSecret", "[REDACTED]");
 
-  static getUrl(network: Network, apiKey: any): ConnectionInfo {
-    let host: string = null;
-    switch (network ? network.name : "unknown") {
-      case "homestead":
-        host = "mainnet.infura.io";
-        break;
-      case "ropsten":
-        host = "ropsten.infura.io";
-        break;
-      case "rinkeby":
-        host = "rinkeby.infura.io";
-        break;
-      case "kovan":
-        host = "kovan.infura.io";
-        break;
-      case "goerli":
-        host = "goerli.infura.io";
-        break;
-      case "matic":
-        host = "polygon-mainnet.infura.io";
-        break;
-      case "maticmum":
-        host = "polygon-mumbai.infura.io";
-        break;
-      case "optimism":
-        host = "optimism-mainnet.infura.io";
-        break;
-      case "optimism-kovan":
-        host = "optimism-kovan.infura.io";
-        break;
-      default:
-        logger.throwError(
-          "unsupported network",
-          Logger.errors.INVALID_ARGUMENT,
-          {
-            argument: "network",
-            value: network,
-          }
-        );
-    }
+            apiKeyObj.projectId = apiKey.projectId;
+            apiKeyObj.projectSecret = apiKey.projectSecret;
 
-    const connection: ConnectionInfo = {
-      allowGzip: true,
-      url: "https:/" + "/" + host + "/v3/" + apiKey.projectId,
-      throttleCallback: (attempt: number, url: string) => {
-        if (apiKey.projectId === defaultProjectId) {
-          showThrottleMessage();
+        } else if (apiKey.projectId) {
+            apiKeyObj.projectId = apiKey.projectId;
         }
-        return Promise.resolve(true);
-      },
-    };
 
-    if (apiKey.projectSecret != null) {
-      connection.user = "";
-      connection.password = apiKey.projectSecret;
+        apiKeyObj.apiKey = apiKeyObj.projectId;
+
+        return apiKeyObj;
     }
 
-    return connection;
-  }
+    static getUrl(network: Network, apiKey: any): ConnectionInfo {
+        let host: string = null;
+        switch(network ? network.name: "unknown") {
+            case "homestead":
+                host = "mainnet.infura.io";
+                break;
+            case "ropsten":
+                host = "ropsten.infura.io";
+                break;
+            case "rinkeby":
+                host = "rinkeby.infura.io";
+                break;
+            case "kovan":
+                host = "kovan.infura.io";
+                break;
+            case "goerli":
+                host = "goerli.infura.io";
+                break;
+            case "matic":
+                host = "polygon-mainnet.infura.io";
+                break;
+            case "maticmum":
+                host = "polygon-mumbai.infura.io";
+                break;
+            case "optimism":
+                host = "optimism-mainnet.infura.io";
+                break;
+            case "optimism-kovan":
+                host = "optimism-kovan.infura.io";
+                break;
+            default:
+                logger.throwError("unsupported network", Logger.errors.INVALID_ARGUMENT, {
+                    argument: "network",
+                    value: network
+                });
+        }
 
-  isCommunityResource(): boolean {
-    return this.projectId === defaultProjectId;
-  }
+        const connection: ConnectionInfo = {
+            allowGzip: true,
+            url: ("https:/" + "/" + host + "/v3/" + apiKey.projectId),
+            throttleCallback: (attempt: number, url: string) => {
+                if (apiKey.projectId === defaultProjectId) {
+                    showThrottleMessage();
+                }
+                return Promise.resolve(true);
+            }
+        };
+
+        if (apiKey.projectSecret != null) {
+            connection.user = "";
+            connection.password = apiKey.projectSecret
+        }
+
+        return connection;
+    }
+
+    isCommunityResource(): boolean {
+        return (this.projectId === defaultProjectId);
+    }
 }
