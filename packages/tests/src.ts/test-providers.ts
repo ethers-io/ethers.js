617714d19632c7b4789f042ef8357f858421fbae
Richard Moore <github@ricmoo.com>
2021-11-30 19:11:44 -0500

    Fixed test case for getAvatar; url has moved

--- packages/tests/src.ts/test-providers.ts
+++ packages/tests/src.ts/test-providers.ts
@@ -1383,13 +1383,13 @@ describe("Resolve ENS avatar", function() {
 
     [
         { title: "ERC-1155", name: "nick.eth", value: "https:/\/lh3.googleusercontent.com/hKHZTZSTmcznonu8I6xcVZio1IF76fq0XmcxnvUykC-FGuVJ75UPdLDlKJsfgVXH9wOSmkyHw0C39VAYtsGyxT7WNybjQ6s3fM3macE" },
-        { title: "ERC-721", name: "brantly.eth", value: "https:/\/wrappedpunks.com:3000/images/punks/2430.png" },
+        { title: "ERC-721", name: "brantly.eth", value: "https:/\/api.wrappedpunks.com/images/punks/2430.png" }
     ].forEach((test) => {
         it(`Resolves avatar for ${ test.title }`, async function() {
             this.timeout(60000);
             const provider = ethers.getDefaultProvider("homestead", getApiKeys("homestead"));
             const avatar = await provider.getAvatar(test.name);
-            assert.equal(test.value, avatar, "avatar url");
+            assert.equal(avatar, test.value, "avatar url");
         });
     });
 });
