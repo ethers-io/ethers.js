fd026eb335be9fd02fac472209d5599a0164a112
Richard Moore <github@ricmoo.com>
2022-04-19 03:34:47 -0400

    Add ABI tests.

---
+++ packages/abi/src.ts/tests/types.ts
@@ -0,0 +1,22 @@
+export type TestCaseAbiVerbose = {
+    type: "address" | "hexstring" | "number" | "string",
+    value: string
+} | {
+    type: "boolean",
+    value: boolean
+} | {
+    type: "array",
+    value: Array<TestCaseAbiVerbose>
+} | {
+    type: "object",
+    value: Array<TestCaseAbiVerbose>
+}
+
+export interface TestCaseAbi {
+    name: string;
+    type: string;
+    value: any;
+    verbose: TestCaseAbiVerbose;
+    bytecode: string;
+    encoded: string;
+}
