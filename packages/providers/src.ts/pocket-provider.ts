50596527fa0087ebf1844881a65f040d56c79a58
blockjoe <69400937+blockjoe@users.noreply.github.com>
2022-05-12 14:23:27 -0400

    Updated the pocket-provider to align with more recent updates

--- packages/providers/src.ts/pocket-provider.ts
+++ packages/providers/src.ts/pocket-provider.ts
@@ -10,47 +10,10 @@ const logger = new Logger(version);
 
 import { UrlJsonRpcProvider } from "./url-json-rpc-provider";
 
-// These are load-balancer-based application IDs
-const defaultApplicationIds: Record<string, string> = {
-    homestead: "6004bcd10040261633ade990",
-    ropsten: "6004bd4d0040261633ade991",
-    rinkeby: "6004bda20040261633ade994",
-    goerli: "6004bd860040261633ade992",
-};
 
 export class PocketProvider extends UrlJsonRpcProvider {
     readonly applicationId: string;
     readonly applicationSecretKey: string;
-    readonly loadBalancer: boolean;
-
-    constructor(network?: Networkish, apiKey?: any) {
-        // We need a bit of creativity in the constructor because
-        // Pocket uses different default API keys based on the network
-
-        if (apiKey == null) {
-            const n = getStatic<(network: Networkish) => Network>(new.target, "getNetwork")(network);
-            if (n) {
-                const applicationId = defaultApplicationIds[n.name];
-                if (applicationId) {
-                    apiKey = {
-                        applicationId: applicationId,
-                        loadBalancer: true
-                    };
-                }
-            }
-
-            // If there was any issue above, we don't know this network
-            if (apiKey == null) {
-                logger.throwError("unsupported network", Logger.errors.INVALID_ARGUMENT, {
-                    argument: "network",
-                    value: network
-                });
-            }
-
-        }
-
-        super(network, apiKey);
-    }
 
     static getApiKey(apiKey: any): any {
         // Most API Providers allow null to get the default configuration, but
@@ -61,9 +24,8 @@ export class PocketProvider extends UrlJsonRpcProvider {
             logger.throwArgumentError("PocketProvider.getApiKey does not support null apiKey", "apiKey", apiKey);
         }
 
-        const apiKeyObj: { applicationId: string, applicationSecretKey: string, loadBalancer: boolean } = {
+        const apiKeyObj: { applicationId: string, applicationSecretKey: string } = {
             applicationId: null,
-            loadBalancer: false,
             applicationSecretKey: null
         };
 
@@ -79,14 +41,12 @@ export class PocketProvider extends UrlJsonRpcProvider {
 
             apiKeyObj.applicationId = apiKey.applicationId;
             apiKeyObj.applicationSecretKey = apiKey.applicationSecretKey;
-            apiKeyObj.loadBalancer = !!apiKey.loadBalancer;
 
         } else if (apiKey.applicationId) {
             logger.assertArgument((typeof (apiKey.applicationId) === "string"),
                 "apiKey.applicationId must be a string", "apiKey.applicationId", apiKey.applicationId);
 
             apiKeyObj.applicationId = apiKey.applicationId;
-            apiKeyObj.loadBalancer = !!apiKey.loadBalancer;
 
         } else {
             logger.throwArgumentError("unsupported PocketProvider apiKey", "apiKey", apiKey);
@@ -110,6 +70,18 @@ export class PocketProvider extends UrlJsonRpcProvider {
             case "goerli":
                 host = "eth-goerli.gateway.pokt.network";
                 break;
+	    case "kovan":
+	    	host = "poa-kovan.gateway.pokt.network";
+	    	break;
+	    case "xdai":
+		host = "gnosischain-mainnet.gateway.pokt.network";
+	    	break;
+	    case "matic":
+	        host = "poly-mainnet.gateway.pokt.network";
+	    	break;
+	    case "bnb":
+	        host = "bsc-mainnet.gateway.pokt.network";
+		break;
             default:
                 logger.throwError("unsupported network", Logger.errors.INVALID_ARGUMENT, {
                     argument: "network",
@@ -117,12 +89,7 @@ export class PocketProvider extends UrlJsonRpcProvider {
                 });
         }
 
-        let url = null;
-        if (apiKey.loadBalancer) {
-            url = `https:/\/${ host }/v1/lb/${ apiKey.applicationId }`
-        } else {
-            url = `https:/\/${ host }/v1/${ apiKey.applicationId }`
-        }
+        const url = `https:/\/${ host }/v1/lb/${ apiKey.applicationId }`
 
         const connection: ConnectionInfo = { url };
 
@@ -139,6 +106,6 @@ export class PocketProvider extends UrlJsonRpcProvider {
     }
 
     isCommunityResource(): boolean {
-        return (this.applicationId === defaultApplicationIds[this.network.name]);
+        return false;
     }
 }
