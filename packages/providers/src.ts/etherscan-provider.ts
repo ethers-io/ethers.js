36615915c166e806b6930b10c82eecc8d1348fe8
xdzurman <kamil.dzurman@gmail.com>
2022-06-23 16:16:35 +0200

    Change etherscan's getHistory query sort to DESC

--- packages/providers/src.ts/etherscan-provider.ts
+++ packages/providers/src.ts/etherscan-provider.ts
@@ -426,7 +426,7 @@ export class EtherscanProvider extends BaseProvider{
             address: (await this.resolveName(addressOrName)),
             startblock: ((startBlock == null) ? 0: startBlock),
             endblock: ((endBlock == null) ? 99999999: endBlock),
-            sort: "asc"
+            sort: "desc"
         };
 
         const result = await this.fetch("account", params);
