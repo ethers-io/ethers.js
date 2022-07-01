043eb86999d32630f715710b7fd663f6bf4995bb
Zainan Victor Zhou <zzn@zzn.im>
2022-07-01 23:34:57 +0000

    Fix compile error by removing unused variable.

--- packages/web/src.ts/browser-geturl.ts
+++ packages/web/src.ts/browser-geturl.ts
@@ -1,6 +1,5 @@
 "use strict";
 
-import { isArrowFunctionExpression } from "@babel/types";
 import { arrayify } from "@ethersproject/bytes";
 
 import type { GetUrlResponse, Options } from "./types";
