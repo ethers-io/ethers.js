d7c62525219076e9c673593aa38c679a82cc2545
Richard Moore <github@ricmoo.com>
2022-09-15 22:20:48 -0400

    tests: added initial provider tests.

--- src.ts/_tests/utils.ts
+++ src.ts/_tests/utils.ts
@@ -30,3 +30,97 @@ export function log(context: any, text: string): void {
         console.log(text);
     }
 }
+
+async function stall(duration: number): Promise<void> {
+    return new Promise((resolve) => { setTimeout(resolve, duration); });
+}
+
+export interface MochaRunnable {
+    timeout: (value: number) => void;
+    skip: () => void;
+}
+
+const ATTEMPTS = 5;
+export async function retryIt(name: string, func: (this: MochaRunnable) => Promise<void>): Promise<void> {
+    it(name, async function() {
+        this.timeout(ATTEMPTS * 5000);
+
+        for (let i = 0; i < ATTEMPTS; i++) {
+            try {
+                await func.call(this);
+                return;
+            } catch (error: any) {
+                if (error.message === "sync skip; aborting execution") {
+                    // Skipping a test; let mocha handle it
+                    throw error;
+                } else if (error.code === "ERR_ASSERTION") {
+                    // Assertion error; let mocha scold us
+                    throw error;
+                } else {
+                    if (i === ATTEMPTS - 1) {
+                        stats.pushRetry(i, name, error);
+                    } else {
+                        await stall(500 * (1 << i));
+                        stats.pushRetry(i, name, null);
+                    }
+                }
+            }
+        }
+
+        // All hope is lost.
+        throw new Error(`Failed after ${ ATTEMPTS } attempts; ${ name }`);
+    });
+}
+
+export interface StatSet {
+    name: string;
+    retries: Array<{ message: string, error: null | Error }>;
+}
+
+const _guard = { };
+
+export class Stats {
+    #stats: Array<StatSet>;
+
+    constructor(guard: any) {
+        if (guard !== _guard) { throw new Error("private constructor"); }
+        this.#stats = [ ];
+    }
+
+    #currentStats(): StatSet {
+        if (this.#stats.length === 0) { throw new Error("no active stats"); }
+        return this.#stats[this.#stats.length - 1];
+    }
+
+    pushRetry(attempt: number, line: string, error: null | Error): void {
+        const { retries } = this.#currentStats();
+
+        if (attempt > 0) { retries.pop(); }
+        if (retries.length < 100) {
+            retries.push({
+                message: `${ attempt + 1 } failures: ${ line }`,
+                error
+            });
+        }
+    }
+
+    start(name: string): void {
+        this.#stats.push({ name, retries: [ ] });
+    }
+
+    end(context?: any): void {
+        let log = console.log.bind(console);
+        if (context && typeof(context._ethersLog) === "function") {
+            log = context._ethersLog;
+        }
+        const { name, retries } = this.#currentStats();
+        if (retries.length === 0) { return; }
+        log(`Warning: The following tests required retries (${ name })`);
+        retries.forEach(({ error, message }) => {
+            log("  " + message);
+            if (error) { log(error); }
+        });
+    }
+}
+
+export const stats = new Stats(_guard);
