26bdf2f93396c9b08b87d01ad24b7219aa8a5216
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2022-09-30 15:41:29 +1000

    Update base64.js
    Removed atob() and btoa() as they have been depreciated

--- packages/base64/lib.esm/base64.js
+++ packages/base64/lib.esm/base64.js
@@ -1,7 +1,8 @@
 "use strict";
 import { arrayify } from "@ethersproject/bytes";
+import { Buffer } from "buffer";
 export function decode(textData) {
-    textData = atob(textData);
+    textData = Buffer.from(textData, 'base64').toString('binary');
     const data = [];
     for (let i = 0; i < textData.length; i++) {
         data.push(textData.charCodeAt(i));
@@ -14,6 +15,7 @@ export function encode(data) {
     for (let i = 0; i < data.length; i++) {
         textData += String.fromCharCode(data[i]);
     }
-    return btoa(textData);
+    let returnData = Buffer.from(textData, 'binary').toString('base64');
+    return returnData;
 }
 //# sourceMappingURL=base64.js.map
