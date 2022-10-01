0fd2a450af5e4683659b132d34a729230ee35459
Richard Moore <github@ricmoo.com>
2022-09-30 20:02:42 -0400

    Add negative hexstring support to getNumber.

--- src.ts/utils/maths.ts
+++ src.ts/utils/maths.ts
@@ -76,6 +76,9 @@ export function getBigInt(value: BigNumberish, name?: string): bigint {
             return BigInt(value);
         case "string":
             try {
+                if (value[0] === "-" && value[1] !== "-") {
+                    return -BigInt(value.substring(1));
+                }
                 return BigInt(value);
             } catch(e: any) {
                 throwArgumentError(`invalid BigNumberish string: ${ e.message }`, name || "value", value);
