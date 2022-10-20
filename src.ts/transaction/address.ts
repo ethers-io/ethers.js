45d29fd3180d87963639fee5a90320892a3a9a41
Richard Moore <github@ricmoo.com>
2022-10-20 04:47:44 -0400

    Allow computing an address from a SigningKey.

--- src.ts/transaction/address.ts
+++ src.ts/transaction/address.ts
@@ -4,10 +4,14 @@ import { keccak256, SigningKey } from "../crypto/index.js";
 import type { SignatureLike } from "../crypto/index.js";
 import type { BytesLike } from "../utils/index.js";
 
-
-export function computeAddress(key: string): string {
-    const publicKey = SigningKey.computePublicKey(key, false);
-    return getAddress(keccak256("0x" + publicKey.substring(4)).substring(26));
+export function computeAddress(key: string | SigningKey): string {
+    let pubkey: string;
+    if (typeof(key) === "string") {
+        pubkey = SigningKey.computePublicKey(key, false);
+    } else {
+        pubkey = key.publicKey;
+    }
+    return getAddress(keccak256("0x" + pubkey.substring(4)).substring(26));
 }
 
 export function recoverAddress(digest: BytesLike, signature: SignatureLike): string {
