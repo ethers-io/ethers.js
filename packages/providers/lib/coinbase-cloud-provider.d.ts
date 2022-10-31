bca0c8a687ebab3815280c5829a44f8d9bd1be90
Lukas Majercak <lukas.majercak@coinbase.com>
2022-10-31 13:09:25 -0700

    Checkout lib code from master

--- packages/providers/lib/coinbase-cloud-provider.d.ts
+++
@@ -1,8 +0,0 @@
-import { Network } from "@ethersproject/networks";
-import { ConnectionInfo } from "@ethersproject/web";
-import { UrlJsonRpcProvider } from "./url-json-rpc-provider";
-export declare class CoinbaseCloudProvider extends UrlJsonRpcProvider {
-    static getApiKey(apiKey: any): any;
-    static getUrl(network: Network, apiKey: any): ConnectionInfo;
-}
-//# sourceMappingURL=coinbase-cloud-provider.d.ts.map
