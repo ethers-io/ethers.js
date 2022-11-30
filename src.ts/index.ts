ee847541be93897e58cd1b250171c9eefd206fe4
Richard Moore <github@ricmoo.com>
2022-11-30 15:43:23 -0500

    docs: added root jsdocs

--- src.ts/index.ts
+++ src.ts/index.ts
@@ -1,4 +1,9 @@
-
+/**
+ *  The Application Programming Interface (API) is the collection of
+ *  functions, classes and types offered by the Ethers library.
+ *
+ *  @_section: api:API Specification
+ */
 import * as ethers from "./ethers.js";
 
 export { ethers };
