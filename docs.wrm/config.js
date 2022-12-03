11077aefec33322b02058763bfdc6c4522a05333
Richard Moore <github@ricmoo.com>
2022-12-02 22:56:29 -0500

    docs: added initial flatworm config

---
+++ docs.wrm/config.js
@@ -0,0 +1,12 @@
+
+export default {
+  title: "ethers",
+  subtitle: "v6-beta",
+
+  srcBaseUrl: "https:/\/github.com/ethers-io/ethers.js/blob/v6-beta-exports/src.ts/{FILENAME}#L{LINENO}",
+
+  codeRoot: "../src.ts/index.ts",
+  links: [
+    "./links.txt",
+  ],
+};
