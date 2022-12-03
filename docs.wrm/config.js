2d4d545d25634706fbebaf00f61548670cf4676c
Richard Moore <github@ricmoo.com>
2022-12-03 15:13:51 -0500

    docs: include current version in docs

--- docs.wrm/config.js
+++ docs.wrm/config.js
@@ -1,7 +1,19 @@
+import { version } from "../lib.esm/_version.js";
+
+const title = "ethers";
+
+const subtitle = (function(version) {
+    const dash = version.indexOf("-");
+    if (dash === -1) { return version; }
+    return version.substring(dash + 1);
+})(version);
 
 export default {
-  title: "ethers",
-  subtitle: "v6-beta",
+  title, subtitle,
+
+  logo: "./logo.svg",
+
+  prefix: "v6-beta",
 
   srcBaseUrl: "https:/\/github.com/ethers-io/ethers.js/blob/v6-beta-exports/src.ts/{FILENAME}#L{LINENO}",
 
