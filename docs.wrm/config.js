04dd5285865df1aec0fa6a082166aa609394a143
Richard Moore <github@ricmoo.com>
2022-12-11 03:04:23 -0500

    docs: added timestamp generation for footer

--- docs.wrm/config.js
+++ docs.wrm/config.js
@@ -1,6 +1,8 @@
 import * as ethers from "../lib.esm/index.js";
 import { version } from "../lib.esm/_version.js";
 
+import { getModifiedTime } from "../lib.esm/_admin/utils/git.js";
+
 const title = "ethers";
 
 const subtitle = (function(version) {
@@ -22,6 +24,9 @@ export default {
   },
 
   srcBaseUrl: "https:/\/github.com/ethers-io/ethers.js/blob/v6-beta-exports/src.ts/{FILENAME}#L{LINENO}",
+  getTimestamp: function(path) {
+      return getModifiedTime(path);
+  },
 
   docRoot: ".",
 
