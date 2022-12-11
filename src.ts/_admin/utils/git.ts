1c5dae81185594a02f919f4dcd0040e6eedb4200
Richard Moore <github@ricmoo.com>
2022-12-11 03:17:19 -0500

    admin: debuggin CI

--- src.ts/_admin/utils/git.ts
+++ src.ts/_admin/utils/git.ts
@@ -24,12 +24,15 @@ export async function getModifiedTime(filename: string): Promise<null | number> 
     if (!result.ok) { throw new Error(`git log error`); }
 
     let log = result.stdout.trim();
+    console.log("LL", log);
     if (!log) { return null; }
 
     for (let line of log.split("\n")) {
         line = line.trim();
+        console.log("L", line);
         if (!line) { break; }
         const match = line.match(/^date:\s+(.*)$/i);
+        console.log("M", match);
         if (match) {
             return (new Date(match[1].trim())).getTime();;
         }
