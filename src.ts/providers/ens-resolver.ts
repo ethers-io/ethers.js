7f49eeef4f205100d48f75857fa7b61df1a09d69
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2023-04-20 13:48:34 -0300

    Fix `getText` description
    Updates the EIP number which was mistyped.

--- src.ts/providers/ens-resolver.ts
+++ src.ts/providers/ens-resolver.ts
@@ -267,7 +267,7 @@ export class EnsResolver {
     }
 
     /**
-     *  Resolves to the EIP-643 text record for %%key%%, or ``null``
+     *  Resolves to the EIP-634 text record for %%key%%, or ``null``
      *  if unconfigured.
      */
     async getText(key: string): Promise<null | string> {
