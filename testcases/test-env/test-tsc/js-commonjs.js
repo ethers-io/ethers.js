795d064058b65251baea0e5cf8759b23ebc9006a
Richard Moore <github@ricmoo.com>
2023-04-28 20:11:35 +0900

    tests: use matrix for env tests

---
+++ testcases/test-env/test-tsc/js-commonjs.js
@@ -0,0 +1,5 @@
+const { ethers } = require("ethers");
+
+console.log("Version:", ethers.version);
+
+console.log(ethers.getAddress("0x0123456789abcdef0123456789abcdef01234567"));
---
+++ testcases/test-env/test-tsc/prepare.cjs
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+function replace(filename, key, value) {
+    const data = fs.readFileSync(filename).toString();
+    data = data.replace(key, value);
+    fs.writeFileSync(filename, data);
+}
+
+// moduleResolution: node, node16, nodenext
+replace("tsconfig.json", "${TS_MODULE_RESOLUTION}", process.argv[2]);
+
+// module: commonjs, es2020
+replace("tsconfig.json", "${TS_MODULE}", process.argv[3]);
+
+// type: module, commonjs
+replace("package.json", "${PKG_TYPE}", process.argv[4]);
+
+// JavaScript
+fs.writeFileSync("index.js", fs.readFileSync("js-" + process.argv[4] + ".js"));
