2c817e5e09ae6324fe612f266fcfd032e5dadf69
Richard Moore <me@ricmoo.com>
2024-01-31 00:36:39 -0500

    docs: fixed jsdocs typo (#4532)

--- src.ts/contract/factory.ts
+++ src.ts/contract/factory.ts
@@ -97,7 +97,7 @@ export class ContractFactory<A extends Array<any> = Array<any>, I = BaseContract
      *  Resolves to the Contract deployed by passing %%args%% into the
      *  constructor.
      *
-     *  This will resovle to the Contract before it has been deployed to the
+     *  This will resolve to the Contract before it has been deployed to the
      *  network, so the [[BaseContract-waitForDeployment]] should be used before
      *  sending any transactions to it.
      */
