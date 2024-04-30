5c8d17a61825fe002bd45ee5b2239bfd8c8ae189
Richard Moore <me@ricmoo.com>
2024-04-30 19:59:39 -0400

    Added matic-amoy to EtherescanProvider (#4711).

--- src.ts/providers/provider-etherscan.ts
+++ src.ts/providers/provider-etherscan.ts
@@ -17,6 +17,7 @@
  *  - Optimism Goerli Testnet (``optimism-goerli``)
  *  - Polygon (``matic``)
  *  - Polygon Mumbai Testnet (``matic-mumbai``)
+ *  - Polygon Amoy Testnet (``matic-amoy``)
  *
  *  @_subsection api/providers/thirdparty:Etherscan  [providers-etherscan]
  */
@@ -176,6 +177,8 @@ export class EtherscanProvider extends AbstractProvider {
                 return "https:/\/api-testnet.bscscan.com";
             case "matic":
                 return "https:/\/api.polygonscan.com";
+            case "matic-amoy":
+                return "https:/\/api-amoy.polygonscan.com";
             case "matic-mumbai":
                 return "https:/\/api-testnet.polygonscan.com";
             case "optimism":
