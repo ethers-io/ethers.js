d2803445b4041aca486e10d45de65e4325c2d701
Richard Moore <me@ricmoo.com>
2024-04-30 21:02:16 -0400

    docs: fix typo in jsdocs (#4643)

--- src.ts/providers/provider.ts
+++ src.ts/providers/provider.ts
@@ -1832,7 +1832,7 @@ function createRemovedLogFilter(log: { blockHash: string, transactionHash: strin
  *  queries.
  *
  *  Each field that is ``null`` matches **any** value, a field that is
- *  a ``string`` must match exactly that value and and ``array`` is
+ *  a ``string`` must match exactly that value and ``array`` is
  *  effectively an ``OR``-ed set, where any one of those values must
  *  match.
  */
