ef3c9bc3afb9e36a073bbd3572e9021a852bd2c4
Richard Moore <me@ricmoo.com>
2024-12-03 15:09:33 -0500

    Fixed bad logic for searching prefetched transactions by hash (#4868).

--- src.ts/providers/provider.ts
+++ src.ts/providers/provider.ts
@@ -711,7 +711,7 @@ export class Block implements BlockParams, Iterable<string> {
                     tx = v;
                     break;
                 } else {
-                    if (v.hash === hash) { continue; }
+                    if (v.hash !== hash) { continue; }
                     tx = v;
                     break;
                 }
