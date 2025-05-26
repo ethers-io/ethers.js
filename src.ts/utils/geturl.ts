fe98f985e38e7540108b20d7966cca3f85d620f3
Richard Moore <me@ricmoo.com>
2025-05-26 17:41:58 -0400

    Explicitly throw error on gunzip failure to prevent uncaught exception (#4873, #4874).

--- src.ts/utils/geturl.ts
+++ src.ts/utils/geturl.ts
@@ -101,11 +101,18 @@ export function createGetUrl(options?: Record<string, any>): FetchGetUrlFunc {
                 });
 
                 resp.on("end", () => {
-                    if (headers["content-encoding"] === "gzip" && body) {
-                        body = getBytes(gunzipSync(body));
-                    }
+                    try {
+                        if (headers["content-encoding"] === "gzip" && body) {
+                            body = getBytes(gunzipSync(body));
+                        }
 
-                    resolve({ statusCode, statusMessage, headers, body });
+                        resolve({ statusCode, statusMessage, headers, body });
+
+                    } catch (error) {
+                        reject(makeError("bad response data", "SERVER_ERROR", {
+                            request: req, info: { response: resp, error }
+                        }));
+                    }
                 });
 
                 resp.on("error", (error) => {
