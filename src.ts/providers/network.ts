986e8272270fa7394a0acd0c15b3bf407dbc1fc4
Richard Moore <me@ricmoo.com>
2025-12-01 22:04:41 -0500

    Added Filecoin network and Ankr endpoints (#5038).

--- src.ts/providers/network.ts
+++ src.ts/providers/network.ts
@@ -406,6 +406,9 @@ function injectCommonNetworks(): void {
     registerEth("bnb", 56, { ensNetwork: 1 });
     registerEth("bnbt", 97, { });
 
+    registerEth("filecoin", 314, { });
+    registerEth("filecoin-calibration", 314159, { });
+
     registerEth("linea", 59144, { ensNetwork: 1 });
     registerEth("linea-goerli", 59140, { });
     registerEth("linea-sepolia", 59141, { });
